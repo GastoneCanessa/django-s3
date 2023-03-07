e38bef2f0bdc13492b51a36232bba73e9c1deb38
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2023-03-07 15:50:46 +0100

    Initial commit

---
+++ project/upload/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from project.storage_backends import PrivateMediaStorage, PublicMediaStorage
+
+
+class Upload(models.Model):
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(storage=PublicMediaStorage())
+
+
+class UploadPrivate(models.Model):
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(storage=PrivateMediaStorage())
